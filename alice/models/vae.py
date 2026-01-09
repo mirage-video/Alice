95c754d922504a4a15391d7acba48bb8c75508b0
wangxia0yu <wangxia0yu@users.noreply.github.com>
2026-01-09 14:30:00 +0100

    Add RMS_norm implementation

--- alice/models/vae.py
+++ alice/models/vae.py
@@ -28,5 +28,23 @@ class CausalConv3d(nn.Conv3d):
         return super().forward(x)
 
 
+class RMS_norm(nn.Module):
+
+    def __init__(self, dim, channel_first=True, images=True, bias=False):
+        super().__init__()
+        broadcastable_dims = (1, 1, 1) if not images else (1, 1)
+        shape = (dim, *broadcastable_dims) if channel_first else (dim,)
+
+        self.channel_first = channel_first
+        self.scale = dim**0.5
+        self.gamma = nn.Parameter(torch.ones(shape))
+        self.bias = nn.Parameter(torch.zeros(shape)) if bias else 0.
+
+    def forward(self, x):
+        return F.normalize(
+            x, dim=(1 if self.channel_first else
+                    -1)) * self.scale * self.gamma + self.bias
+
+
 class AliceVAE:
     pass
