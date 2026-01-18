1b9d47e20a6a12e6c149471598164e16cf538c76
wangxia0yu <wangxia0yu@users.noreply.github.com>
2026-01-18 11:00:00 +0100

    Implement T5LayerNorm

--- alice/models/text_encoder.py
+++ alice/models/text_encoder.py
@@ -13,3 +13,19 @@ __all__ = [
     'T5Decoder',
     'T5EncoderModel',
 ]
+
+
+class T5LayerNorm(nn.Module):
+
+    def __init__(self, dim, eps=1e-6):
+        super(T5LayerNorm, self).__init__()
+        self.dim = dim
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def forward(self, x):
+        x = x * torch.rsqrt(x.float().pow(2).mean(dim=-1, keepdim=True) +
+                            self.eps)
+        if self.weight.dtype in [torch.float16, torch.bfloat16]:
+            x = x.type_as(self.weight)
+        return self.weight * x
