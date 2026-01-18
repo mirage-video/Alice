385a25c2a5cd174999e98ff1bb92d257e2f50580
wangxia0yu <wangxia0yu@users.noreply.github.com>
2026-01-18 10:15:00 +0100

    Create text_encoder.py skeleton

---
+++ alice/models/text_encoder.py
@@ -0,0 +1,15 @@
+import logging
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from .tokenizers import HuggingfaceTokenizer
+
+__all__ = [
+    'T5Model',
+    'T5Encoder',
+    'T5Decoder',
+    'T5EncoderModel',
+]
