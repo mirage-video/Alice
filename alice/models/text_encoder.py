6279003e6dab2c9d9abcca48118c4142feb13001
wangxia0yu <wangxia0yu@users.noreply.github.com>
2026-01-18 14:30:00 +0100

    Add GELU activation

--- alice/models/text_encoder.py
+++ alice/models/text_encoder.py
@@ -15,6 +15,13 @@ __all__ = [
 ]
 
 
+class GELU(nn.Module):
+
+    def forward(self, x):
+        return 0.5 * x * (1.0 + torch.tanh(
+            math.sqrt(2.0 / math.pi) * (x + 0.044715 * torch.pow(x, 3.0))))
+
+
 class T5LayerNorm(nn.Module):
 
     def __init__(self, dim, eps=1e-6):
