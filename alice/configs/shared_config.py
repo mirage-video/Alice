3af15b7dbaea02d03b728a7429a7bddcbb16223c
chenseris <chenseris@users.noreply.github.com>
2026-01-22 09:30:00 +0100

    Update shared_config for T5

--- alice/configs/shared_config.py
+++ alice/configs/shared_config.py
@@ -3,6 +3,10 @@ from easydict import EasyDict
 
 alice_shared_cfg = EasyDict()
 
+alice_shared_cfg.t5_model = 'umt5_xxl'
+alice_shared_cfg.t5_dtype = torch.bfloat16
+alice_shared_cfg.text_len = 512
+
 alice_shared_cfg.param_dtype = torch.bfloat16
 
 alice_shared_cfg.vae_stride = (4, 8, 8)
