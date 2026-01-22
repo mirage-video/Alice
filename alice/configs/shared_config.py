5d488d3dd8006abbd9ae3a79fe550dbc72433659
chenseris <chenseris@users.noreply.github.com>
2026-01-22 10:00:00 +0100

    Add negative prompt config

--- alice/configs/shared_config.py
+++ alice/configs/shared_config.py
@@ -9,4 +9,7 @@ alice_shared_cfg.text_len = 512
 
 alice_shared_cfg.param_dtype = torch.bfloat16
 
-alice_shared_cfg.vae_stride = (4, 8, 8)
+alice_shared_cfg.num_train_timesteps = 1000
+alice_shared_cfg.sample_fps = 16
+alice_shared_cfg.sample_neg_prompt = '色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走'
+alice_shared_cfg.frame_num = 81
