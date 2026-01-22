f8c471577002f061f61b0116d63766508225976c
chenseris <chenseris@users.noreply.github.com>
2026-01-22 10:30:00 +0100

    Update models __init__.py

--- alice/models/__init__.py
+++ alice/models/__init__.py
@@ -1,7 +1,14 @@
+from .text_encoder import T5Decoder, T5Encoder, T5EncoderModel, T5Model
+from .tokenizers import HuggingfaceTokenizer
 from .vae import AliceVAE
 from .vae22 import AliceVAE22
 
 __all__ = [
     'AliceVAE',
     'AliceVAE22',
+    'T5Model',
+    'T5Encoder',
+    'T5Decoder',
+    'T5EncoderModel',
+    'HuggingfaceTokenizer',
 ]
