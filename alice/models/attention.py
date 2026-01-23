ac0a5ae912d8ef08735b59752ea4de93e53c1ad3
wangxia0yu <wangxia0yu@users.noreply.github.com>
2026-01-23 10:15:00 +0100

    Add flash_attn import handling

--- alice/models/attention.py
+++ alice/models/attention.py
@@ -1,3 +1,15 @@
 import torch
 
+try:
+    import flash_attn_interface
+    FLASH_ATTN_3_AVAILABLE = True
+except ModuleNotFoundError:
+    FLASH_ATTN_3_AVAILABLE = False
+
+try:
+    import flash_attn
+    FLASH_ATTN_2_AVAILABLE = True
+except ModuleNotFoundError:
+    FLASH_ATTN_2_AVAILABLE = False
+
 __all__ = []
