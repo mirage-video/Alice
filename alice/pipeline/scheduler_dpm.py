2caa5246b850eef68618fec0f616b57e3265cede
wangxia0yu <wangxia0yu@users.noreply.github.com>
2026-02-06 10:15:00 +0100

    Add scheduler_dpm.py skeleton

---
+++ alice/pipeline/scheduler_dpm.py
@@ -0,0 +1,14 @@
+"""DPM++ solver for flow-matching diffusion models."""
+
+import math
+from typing import List, Optional, Tuple, Union
+
+import numpy as np
+import torch
+
+
+class FlowDPMSolverMultistepScheduler:
+    """DPM++ solver for flow-matching diffusion models."""
+
+    def __init__(self):
+        pass
