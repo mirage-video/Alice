9097defb7f7945912a4411f7e35ec0e14cd787b2
chenseris <chenseris@users.noreply.github.com>
2026-02-16 09:30:00 +0100

    Add fsdp.py skeleton

---
+++ alice/distributed/fsdp.py
@@ -0,0 +1,4 @@
+import torch
+from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
+from torch.distributed.fsdp import MixedPrecision, ShardingStrategy
+from torch.distributed.fsdp.wrap import lambda_auto_wrap_policy
