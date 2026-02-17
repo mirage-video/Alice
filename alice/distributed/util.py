3bcade524b66fb70559e7491e8729e0037917f6b
chenseris <chenseris@users.noreply.github.com>
2026-02-17 11:00:00 +0100

    Add get_rank, get_world_size

--- alice/distributed/util.py
+++ alice/distributed/util.py
@@ -6,3 +6,11 @@ def init_distributed_group():
     """Initialize distributed process group."""
     if not dist.is_initialized():
         dist.init_process_group(backend='nccl')
+
+
+def get_rank():
+    return dist.get_rank()
+
+
+def get_world_size():
+    return dist.get_world_size()
