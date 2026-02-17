3915b8846cd369c07a7369bb0a89e79b5fe5d339
chenseris <chenseris@users.noreply.github.com>
2026-02-17 14:30:00 +0100

    Implement all_to_all

--- alice/distributed/util.py
+++ alice/distributed/util.py
@@ -14,3 +14,14 @@ def get_rank():
 
 def get_world_size():
     return dist.get_world_size()
+
+
+def all_to_all(x, scatter_dim, gather_dim, group=None, **kwargs):
+    """All-to-all communication: scatter on one dim, gather on another."""
+    world_size = get_world_size()
+    if world_size > 1:
+        inputs = [u.contiguous() for u in x.chunk(world_size, dim=scatter_dim)]
+        outputs = [torch.empty_like(u) for u in inputs]
+        dist.all_to_all(outputs, inputs, group=group, **kwargs)
+        x = torch.cat(outputs, dim=gather_dim).contiguous()
+    return x
