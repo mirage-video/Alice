f18e5cf0cea3e5debc1619a3775ce229164a5b70
chenseris <chenseris@users.noreply.github.com>
2026-02-18 14:30:00 +0100

    Implement pad_freqs

--- alice/distributed/sequence_parallel.py
+++ alice/distributed/sequence_parallel.py
@@ -3,3 +3,16 @@ import torch.cuda.amp as amp
 
 from ..models.transformer import sinusoidal_embedding_1d
 from .util import gather_forward, get_rank, get_world_size
+
+
+def pad_freqs(original_tensor, target_len):
+    seq_len, s1, s2 = original_tensor.shape
+    pad_size = target_len - seq_len
+    padding_tensor = torch.ones(
+        pad_size,
+        s1,
+        s2,
+        dtype=original_tensor.dtype,
+        device=original_tensor.device)
+    padded_tensor = torch.cat([original_tensor, padding_tensor], dim=0)
+    return padded_tensor
