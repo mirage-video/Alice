e1a76f1d61209cd49165b3c4c927cd168b12fcbf
wangxia0yu <wangxia0yu@users.noreply.github.com>
2026-02-24 11:00:00 +0100

    Implement generate function

--- scripts/generate.py
+++ scripts/generate.py
@@ -179,5 +179,149 @@ def _parse_args():
     return args
 
 
+def _init_logging(rank):
+    if rank == 0:
+        logging.basicConfig(
+            level=logging.INFO,
+            format="[%(asctime)s] %(levelname)s: %(message)s",
+            handlers=[logging.StreamHandler(stream=sys.stdout)])
+    else:
+        logging.basicConfig(level=logging.ERROR)
+
+
+def generate(args):
+    rank = int(os.getenv("RANK", 0))
+    world_size = int(os.getenv("WORLD_SIZE", 1))
+    local_rank = int(os.getenv("LOCAL_RANK", 0))
+    device = local_rank
+    _init_logging(rank)
+
+    if args.offload_model is None:
+        args.offload_model = False if world_size > 1 else True
+        logging.info(
+            f"offload_model is not specified, set to {args.offload_model}.")
+    if world_size > 1:
+        torch.cuda.set_device(local_rank)
+        dist.init_process_group(
+            backend="nccl",
+            init_method="env://",
+            rank=rank,
+            world_size=world_size)
+    else:
+        assert not (
+            args.t5_fsdp or args.dit_fsdp
+        ), f"t5_fsdp and dit_fsdp are not supported in non-distributed environments."
+        assert not (
+            args.ulysses_size > 1
+        ), f"sequence parallel are not supported in non-distributed environments."
+
+    if args.ulysses_size > 1:
+        assert args.ulysses_size == world_size, f"The number of ulysses_size should be equal to the world size."
+        init_distributed_group()
+
+    if args.use_prompt_extend:
+        if args.prompt_extend_method == "dashscope":
+            prompt_expander = DashScopePromptExpander(
+                model_name=args.prompt_extend_model,
+                task=args.task,
+                is_vl=False)
+        elif args.prompt_extend_method == "local_qwen":
+            prompt_expander = QwenPromptExpander(
+                model_name=args.prompt_extend_model,
+                task=args.task,
+                is_vl=False,
+                device=rank)
+        else:
+            raise NotImplementedError(
+                f"Unsupport prompt_extend_method: {args.prompt_extend_method}")
+
+    cfg = ALICE_CONFIGS[args.task]
+    if args.ulysses_size > 1:
+        assert cfg.num_heads % args.ulysses_size == 0, f"`{cfg.num_heads=}` cannot be divided evenly by `{args.ulysses_size=}`."
+
+    logging.info(f"Generation job args: {args}")
+    logging.info(f"Generation model config: {cfg}")
+
+    if dist.is_initialized():
+        base_seed = [args.base_seed] if rank == 0 else [None]
+        dist.broadcast_object_list(base_seed, src=0)
+        args.base_seed = base_seed[0]
+
+    logging.info(f"Input prompt: {args.prompt}")
+
+    if args.use_prompt_extend:
+        logging.info("Extending prompt ...")
+        if rank == 0:
+            prompt_output = prompt_expander(
+                args.prompt,
+                tar_lang=args.prompt_extend_target_lang,
+                seed=args.base_seed)
+            if prompt_output.status == False:
+                logging.info(
+                    f"Extending prompt failed: {prompt_output.message}")
+                logging.info("Falling back to original prompt.")
+                input_prompt = args.prompt
+            else:
+                input_prompt = prompt_output.prompt
+            input_prompt = [input_prompt]
+        else:
+            input_prompt = [None]
+        if dist.is_initialized():
+            dist.broadcast_object_list(input_prompt, src=0)
+        args.prompt = input_prompt[0]
+        logging.info(f"Extended prompt: {args.prompt}")
+
+    logging.info("Creating AliceTextToVideo pipeline.")
+    alice_t2v = alice.AliceTextToVideo(
+        config=cfg,
+        checkpoint_dir=args.ckpt_dir,
+        device_id=device,
+        rank=rank,
+        t5_fsdp=args.t5_fsdp,
+        dit_fsdp=args.dit_fsdp,
+        use_sp=(args.ulysses_size > 1),
+        t5_cpu=args.t5_cpu,
+        convert_model_dtype=args.convert_model_dtype,
+    )
+
+    logging.info(f"Generating video ...")
+    video = alice_t2v.generate(
+        args.prompt,
+        size=SIZE_CONFIGS[args.size],
+        frame_num=args.frame_num,
+        shift=args.sample_shift,
+        sample_solver=args.sample_solver,
+        sampling_steps=args.sample_steps,
+        guide_scale=args.sample_guide_scale,
+        seed=args.base_seed,
+        offload_model=args.offload_model)
+
+    if rank == 0:
+        if args.save_file is None:
+            formatted_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+            formatted_prompt = args.prompt.replace(" ", "_").replace("/",
+                                                                     "_")[:50]
+            suffix = '.mp4'
+            args.save_file = f"{args.task}_{args.size.replace('*','x') if sys.platform=='win32' else args.size}_{args.ulysses_size}_{formatted_prompt}_{formatted_time}" + suffix
+
+        logging.info(f"Saving generated video to {args.save_file}")
+        save_video(
+            tensor=video[None],
+            save_file=args.save_file,
+            fps=cfg.sample_fps,
+            nrow=1,
+            normalize=True,
+            value_range=(-1, 1))
+    del video
+
+    torch.cuda.synchronize()
+    if dist.is_initialized():
+        dist.barrier()
+        dist.destroy_process_group()
+
+    logging.info("Finished.")
+
+
 if __name__ == "__main__":
     args = _parse_args()
+    generate(args)
