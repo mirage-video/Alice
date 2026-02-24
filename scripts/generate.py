37e1baa13a1d45546d1963eaa9eb387c0df11765
chenseris <chenseris@users.noreply.github.com>
2026-02-24 10:15:00 +0100

    Add --use_prompt_extend

--- scripts/generate.py
+++ scripts/generate.py
@@ -15,6 +15,9 @@ from PIL import Image
 
 import alice
 from alice.configs import ALICE_CONFIGS, MAX_AREA_CONFIGS, SIZE_CONFIGS, SUPPORTED_SIZES
+from alice.distributed.util import init_distributed_group
+from alice.utils.prompt_extend import DashScopePromptExpander, QwenPromptExpander
+from alice.utils.utils import save_video, str2bool
 
 
 EXAMPLE_PROMPT = {
@@ -35,8 +38,20 @@ def _validate_args(args):
 
     cfg = ALICE_CONFIGS[args.task]
 
+    if args.sample_steps is None:
+        args.sample_steps = cfg.sample_steps
+
+    if args.sample_shift is None:
+        args.sample_shift = cfg.sample_shift
+
+    if args.sample_guide_scale is None:
+        args.sample_guide_scale = cfg.sample_guide_scale
+
     if args.frame_num is None:
         args.frame_num = cfg.frame_num
+
+    args.base_seed = args.base_seed if args.base_seed >= 0 else random.randint(
+        0, sys.maxsize)
 
     assert args.size in SUPPORTED_SIZES[
         args.
@@ -101,6 +116,62 @@ def _parse_args():
         type=str,
         default=None,
         help="The file to save the generated video to.")
+    parser.add_argument(
+        "--offload_model",
+        type=str2bool,
+        default=None,
+        help="Whether to offload the model to CPU after each model forward, reducing GPU memory usage."
+    )
+    parser.add_argument(
+        "--use_prompt_extend",
+        action="store_true",
+        default=False,
+        help="Whether to use prompt extend.")
+    parser.add_argument(
+        "--prompt_extend_method",
+        type=str,
+        default="local_qwen",
+        choices=["dashscope", "local_qwen"],
+        help="The prompt extend method to use.")
+    parser.add_argument(
+        "--prompt_extend_model",
+        type=str,
+        default=None,
+        help="The prompt extend model to use.")
+    parser.add_argument(
+        "--prompt_extend_target_lang",
+        type=str,
+        default="zh",
+        choices=["zh", "en"],
+        help="The target language of prompt extend.")
+    parser.add_argument(
+        "--base_seed",
+        type=int,
+        default=-1,
+        help="The seed to use for generating the video.")
+    parser.add_argument(
+        "--sample_solver",
+        type=str,
+        default='unipc',
+        choices=['unipc', 'dpm++'],
+        help="The solver used to sample.")
+    parser.add_argument(
+        "--sample_steps", type=int, default=None, help="The sampling steps.")
+    parser.add_argument(
+        "--sample_shift",
+        type=float,
+        default=None,
+        help="Sampling shift factor for flow matching schedulers.")
+    parser.add_argument(
+        "--sample_guide_scale",
+        type=float,
+        default=None,
+        help="Classifier free guidance scale.")
+    parser.add_argument(
+        "--convert_model_dtype",
+        action="store_true",
+        default=False,
+        help="Whether to convert model paramerters dtype.")
 
     args = parser.parse_args()
     _validate_args(args)
