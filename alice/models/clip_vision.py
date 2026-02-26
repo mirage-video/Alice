e50df2e58af41bd01469eccbd63add35e181cbac
wangxia0yu <wangxia0yu@users.noreply.github.com>
2026-02-26 10:15:00 +0100

    Implement QuickGELU activation

--- alice/models/clip_vision.py
+++ alice/models/clip_vision.py
@@ -11,5 +11,11 @@ from einops import rearrange
 __all__ = []
 
 
+class QuickGELU(nn.Module):
+
+    def forward(self, x: torch.Tensor):
+        return x * torch.sigmoid(1.702 * x)
+
+
 class CLIPVisionEncoder:
     pass
