aaf3f9989a6165be7553e674cbe7258d6fd5f44a
phongnhhn92 <phongnhhn92@users.noreply.github.com>
2026-02-26 09:30:00 +0100

    Create clip_vision.py skeleton

---
+++ alice/models/clip_vision.py
@@ -0,0 +1,15 @@
+import logging
+from collections import OrderedDict
+from typing import Optional, Tuple
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange
+
+
+__all__ = []
+
+
+class CLIPVisionEncoder:
+    pass
