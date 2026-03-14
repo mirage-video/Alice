c4e00d4c9d8c6959fbb2be0ee765fef15d864e38
phongnhhn92 <phongnhhn92@users.noreply.github.com>
2026-03-14 10:15:00 +0100

    Update quantization __init__.py

--- alice/quantization/__init__.py
+++ alice/quantization/__init__.py
@@ -1,1 +1,14 @@
-__all__ = []
+from .fp8_linear import FP8Linear, quantize_model, dequantize_model
+from .calibration import CalibrationContext, collect_calibration_stats
+from .utils import compute_scale, per_tensor_quantize, per_channel_quantize
+
+__all__ = [
+    'FP8Linear',
+    'quantize_model',
+    'dequantize_model',
+    'CalibrationContext',
+    'collect_calibration_stats',
+    'compute_scale',
+    'per_tensor_quantize',
+    'per_channel_quantize',
+]
