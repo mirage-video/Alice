00fe2db53d583feab65637ebe0137ebdb12c2e3d
phongnhhn92 <phongnhhn92@users.noreply.github.com>
2026-03-19 09:30:00 +0100

    Create vae_tiling.py skeleton

---
+++ alice/models/vae_tiling.py
@@ -0,0 +1,14 @@
+import logging
+from typing import List, Optional, Tuple
+
+import torch
+import torch.nn.functional as F
+
+from .vae import AliceVAE
+
+
+__all__ = ['TiledVAE']
+
+
+class TiledVAE:
+    pass
