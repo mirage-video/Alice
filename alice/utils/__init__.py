dd4f069a4003f0dbb9d7bc4491ff9dfeefa6dbf6
phongnhhn92 <phongnhhn92@users.noreply.github.com>
2026-04-25 11:00:00 +0200

    Update utils __init__.py exports

--- alice/utils/__init__.py
+++ alice/utils/__init__.py
@@ -1,1 +1,47 @@
-__all__ = []
+from .memory import (
+    get_gpu_memory_info,
+    estimate_model_memory,
+    estimate_inference_memory,
+    MemoryTracker,
+    clear_gpu_memory,
+    log_memory_stats,
+)
+from .compile import (
+    compile_model,
+    compile_vae,
+    compile_transformer,
+    DynamicShapeGuard,
+    GraphBreakAnalyzer,
+)
+from .checkpoint import (
+    save_safetensors,
+    load_safetensors,
+    load_checkpoint_auto,
+    convert_to_safetensors,
+    shard_checkpoint,
+    merge_sharded_checkpoint,
+)
+from .cache import KVCache, PagedKVCache, StaticKVCache
+
+__all__ = [
+    'get_gpu_memory_info',
+    'estimate_model_memory',
+    'estimate_inference_memory',
+    'MemoryTracker',
+    'clear_gpu_memory',
+    'log_memory_stats',
+    'compile_model',
+    'compile_vae',
+    'compile_transformer',
+    'DynamicShapeGuard',
+    'GraphBreakAnalyzer',
+    'save_safetensors',
+    'load_safetensors',
+    'load_checkpoint_auto',
+    'convert_to_safetensors',
+    'shard_checkpoint',
+    'merge_sharded_checkpoint',
+    'KVCache',
+    'PagedKVCache',
+    'StaticKVCache',
+]
